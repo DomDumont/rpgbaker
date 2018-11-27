b1e0f6f7519104bc150945cb3a3b28cec8565350
DomDom <dom.dumont@gmail.com>
2018-11-27 14:53:25 +0100

    cleaning

--- src/camera.ts
+++ src/camera.ts
@@ -1,31 +1,43 @@
+import { GameObject } from './gameobject'
+import { Game } from '.'
+
 /** Camera Class  */
 export class Camera {
-  myParent: any
-  gaoToFollow: any
-  borderX: any
-  borderY: any
+  game: Game
+  gaoToFollow: GameObject
+  borderX: number
+  borderY: number
 
   constructor (myParent: any) {
-    this.myParent = myParent
-    this.gaoToFollow = undefined
+    this.game = myParent
+    this.gaoToFollow = null
     this.borderX = 200
     this.borderY = 100
   }
+
+  /**
+   *  Tells the Camera to follow a specific GameObject
+   * @param gameObject  the GameObject to follow
+   */
   Follow (gameObject: any) {
     this.gaoToFollow = gameObject
     this.Update(0) // To avoid glitches, bad
   }
 
+  /**
+   * Update the position of the camera if any object must be followed
+   * @param delta
+   */
   Update (delta: any) {
     if (this.gaoToFollow) {
-      this.myParent.app.stage.position.x =
-        -this.gaoToFollow.x + this.myParent.app.renderer.width / 2
+      this.game.app.stage.position.x =
+        -this.gaoToFollow.x + this.game.app.renderer.width / 2
 
-      this.myParent.app.stage.position.y =
-        -this.gaoToFollow.y + this.myParent.app.renderer.height / 2
+      this.game.app.stage.position.y =
+        -this.gaoToFollow.y + this.game.app.renderer.height / 2
     } else {
-      this.myParent.app.stage.position.x = 0
-      this.myParent.app.stage.position.y = 0
+      this.game.app.stage.position.x = 0
+      this.game.app.stage.position.y = 0
     }
   }
 }
